70d20066869bbc65d7c9d5ba3082441d317baf69
Berny Zamora <berny.zamora@gmail.com>
2013-06-29 14:40:06 -0500

    init commit

---
+++ MyToDo/js/MyToDo.js
@@ -0,0 +1,151 @@
+﻿var MyToDo = MyToDo || {};
+
+(function (MyToDo) {
+    "use strict";
+
+    MyToDo.data = (function () {
+        var indexedDB = window.indexedDB;
+
+        var init = function (success) {
+            var request = indexedDB.open("MyToDo", 1);
+
+            request.onsuccess = function () {
+                MyToDo.data.db = request.result;
+                success();
+            };
+
+            request.onerror = function (event) {
+                console.log(event);
+            };
+
+            request.onblocked = function (event) {
+                console.log(event);
+            };
+
+            request.onupgradeneeded = function (e) {
+                var db = e.target.result;
+
+                var toDoStore = db.createObjectStore("toDos", {
+                    keyPath: "id",
+                    autoIncrement: true
+                });
+
+                toDoStore.createIndex("name", "name", {
+                    unique: false
+                });
+            };
+        };
+
+        return {
+            init: init
+        };
+
+    })();
+
+    MyToDo.toDoList = (function () {
+
+        var getList = function (success) {
+            var
+                list = [],
+                transaction = MyToDo.data.db.transaction("toDos"),
+                store = transaction.objectStore("toDos");
+
+            store.openCursor().onsuccess = function (e) {
+                var cursor = e.target.result;
+
+                if (cursor) {
+                    list.push(cursor.value);
+                    cursor.continue();
+                }
+                else {
+                    success(list);
+                }
+            };
+        };
+
+        var search = function (term, success) {
+            var
+                list = [],
+                transaction = MyToDo.data.db.transaction("toDos"),
+                store = transaction.objectStore("toDos"),
+                range = IDBKeyRange.bound(term, term + '\uffff'),
+                index = store.index("name");
+
+            index.openCursor(range).onsuccess = function (e) {
+                var cursor = e.target.result;
+
+                if (cursor) {
+                    list.push(cursor.value);
+                    cursor.continue();
+                }
+                else {
+                    success(list);
+                }
+            };
+        };
+
+        var get = function (key, success) {
+            var
+                transaction = MyToDo.data.db.transaction("toDos"),
+                store = transaction.objectStore("toDos"),
+                request = store.get(key);
+
+            request.onsuccess = function (e) {
+                success(e.target.result);
+            };
+        };
+
+        var add = function (toDo, success) {
+            var
+                transaction = MyToDo.data.db.transaction("toDos", "readwrite"),
+                store = transaction.objectStore("toDos"),
+                request = store.add({
+                    name: toDo.name,
+                    description: toDo.description
+                });
+
+            request.onsuccess = function (e) {
+                toDo.id = e.target.result;
+                success(toDo);
+            };
+        };
+
+        var update = function (toDo, success) {
+            var
+                transaction = MyToDo.data.db.transaction("toDos", "readwrite"),
+                store = transaction.objectStore("toDos"),
+                request = store.put({
+                    id: parseInt(toDo.id, 10),
+                    name: toDo.name,
+                    description: toDo.description
+                });
+
+            request.onsuccess = function () {
+                success(toDo);
+            };
+        };
+
+        var remove = function (key, success) {
+            var
+                transaction = MyToDo.data.db.transaction("toDos", "readwrite"),
+                store = transaction.objectStore("toDos"),
+                request = store.delete(key);
+
+            request.onsuccess = function () {
+                success();
+            };
+        };
+
+        return {
+            get: get,
+            getList: getList,
+            search : search,
+            add: add,
+            update: update,
+            remove: remove
+        };
+
+    })();
+
+})(MyToDo);
+
---
+++ MyToDo/js/default.js
@@ -0,0 +1,203 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            args.setPromise(WinJS.UI.processAll().then(function () {
+                initializeDb().done(function () {
+                    initializeUI();
+                });
+            }));
+        }
+    };
+
+    app.start();
+
+    function initializeDb() {
+        return new WinJS.Promise(function (completed, error, progress) {
+            MyToDo.data.init(function () {
+                completed();
+            });
+        });
+    }
+
+    function initializeUI() {
+        var viewModel = new ViewModel();
+        viewModel.init();
+
+        document.getElementById("addForm").addEventListener("submit", viewModel.submitAdd, false);
+        document.getElementById("editForm").addEventListener("submit", viewModel.submitEdit, false);
+        document.getElementById("deleteCommand").addEventListener("click", viewModel.deleteToDo, false);
+        document.getElementById("editCommand").addEventListener("click", viewModel.editToDo, false);
+        document.getElementById("searchInput").addEventListener("keyup", viewModel.search, false);
+        document.querySelector("#editForm .cancel").addEventListener("click", viewModel.cancelEdit, false);
+    }
+
+    function ViewModel() {
+        var
+            listView = document.getElementById("toDoList").winControl,
+            appBar = document.getElementById("appBar").winControl,
+            editFlyout = document.getElementById("editFlyout").winControl,
+            addForm = document.getElementById("addForm"),
+            editForm = document.getElementById("editForm"),
+            self = this,
+            dataList;
+
+        this.init = function () {
+            MyToDo.toDoList.getList(function (e) {
+                dataList = new WinJS.Binding.List(e);
+
+                listView.itemDataSource = dataList.dataSource;
+                listView.onselectionchanged = self.selectionChanged;
+            });
+        };
+
+        this.search = function () {
+            delay(function () {
+                var searchValue = document.getElementById("searchInput").value;
+
+                MyToDo.toDoList.search(searchValue, function (e) {
+                    dataList = new WinJS.Binding.List(e);
+                    listView.itemDataSource = dataList.dataSource;
+                });
+            }, 500);
+        };
+
+        var delay = (function () {
+            var timer = 0;
+            return function (callback, ms) {
+                clearTimeout(timer);
+                timer = setTimeout(callback, ms);
+            };
+        })();
+
+        this.selectionChanged = function (args) {
+            var
+                selectionCount = listView.selection.count(),
+                selectionCommands = document.querySelectorAll(".appBarSelection"),
+                singleSelectionCommands = document.querySelectorAll(".appBarSingleSelection");
+
+            if (selectionCount > 0) {
+                appBar.showCommands(selectionCommands);
+
+                if (selectionCount > 1) {
+                    appBar.hideCommands(singleSelectionCommands);
+                }
+
+                appBar.sticky = true;
+                appBar.show();
+            }
+            else {
+                appBar.hideCommands(selectionCommands);
+
+                appBar.sticky = false;
+                appBar.hide();
+            }
+        };
+
+        this.deleteToDo = function () {
+            var dialog = new Windows.UI.Popups.MessageDialog("Are you sure you want to delete?");
+
+            dialog.commands.append(new Windows.UI.Popups.UICommand("OK", function (command) {
+                var selectionCount = listView.selection.count();
+                if (selectionCount > 0) {
+                    listView.selection.getItems().then(function (items) {
+                        items.forEach(function (item) {
+                            var
+                                dbKey = item.data.id,
+                                lvKey = item.key;
+
+                            MyToDo.toDoList.remove(dbKey, function () {
+                                listView.itemDataSource.remove(lvKey);
+                            });
+                        });
+                    });
+                }
+            }));
+
+            dialog.commands.append(new Windows.UI.Popups.UICommand("Cancel", null));
+
+            dialog.defaultCommandIndex = 1;
+            dialog.cancelCommandIndex = 1;
+
+            dialog.showAsync();
+        };
+
+        this.editToDo = function () {
+            var
+                anchor = document.querySelector(".toDo"),
+                selectionCount = listView.selection.count();
+
+            if (selectionCount === 1) {
+                listView.selection.getItems().then(function (items) {
+                    var
+                        item = items[0],
+                        editFlyoutElement = document.getElementById("editFlyout");
+
+                    var toDo = {
+                        id: item.data.id,
+                        name: item.data.name,
+                        description: item.data.description,
+                        lvIndex: item.index
+                    };
+
+                    var process = WinJS.Binding.processAll(editFlyoutElement, toDo);
+
+                    process.then(function () {
+                        editFlyout.show(anchor, "top", "center");
+                    });
+                });
+            }
+        };
+
+        this.submitAdd = function (e) {
+            e.preventDefault();
+
+            var toDo = {
+                name: document.querySelector("#addForm .name").value,
+                description: document.querySelector("#addForm .description").value
+            };
+
+            MyToDo.toDoList.add(toDo, function (e) {
+                dataList.push(e);
+
+                addForm.reset();
+            });
+        };
+
+        this.submitEdit = function (e) {
+            e.preventDefault();
+
+            var toDo = {
+                id: document.querySelector("#editForm .id").value,
+                name: document.querySelector("#editForm .name").value,
+                description: document.querySelector("#editForm .description").value,
+                lvIndex: document.querySelector("#editForm .lvIndex").value
+            };
+
+            MyToDo.toDoList.update(toDo, function (e) {
+                editFlyout.hide();
+                appBar.hide();
+                editForm.reset();
+                listView.selection.clear();
+
+                dataList.setAt(toDo.lvIndex, toDo);
+            });
+        };
+
+        this.cancelEdit = function (e) {
+            e.preventDefault();
+
+            editFlyout.hide();
+            appBar.hide();
+            editForm.reset();
+            listView.selection.clear();
+        };
+    }
+
+})();
